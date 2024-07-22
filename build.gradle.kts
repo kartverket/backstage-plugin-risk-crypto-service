942673400f84073ce798cd61a87a4f9353cc18a9
Torbjørn Tessem <torbjorn.tessem@kartverket.no>
2024-07-22 10:34:20 +0200

    Konfigurert helsesjekk og metrikker, opprettet initiell konfiugurasjon for å kunne kjøre på SKIP

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+	implementation("io.micrometer:micrometer-registry-prometheus")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
