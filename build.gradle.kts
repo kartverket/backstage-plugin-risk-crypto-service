9aa4332ca756efb08d0519807a6ebc84fc65e8ce
Torbjørn Tessem <torbjorn.tessem@kartverket.no>
2024-07-30 13:07:45 +0200

    Added dependabot.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.springframework.boot") version "3.2.7"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
