96cf13fcd30d7a3b6ed88bb6e9398ba4a67923ba
Maren Sofie Ringsby <maren.sofie.ringsby@bekk.no>
2024-08-06 12:32:06 +0200

    chore: legg til test for når man ikke skal få kunne kryptere

--- src/test/kotlin/cryptoservice/service/EncryptionServiceTest.kt
+++ src/test/kotlin/cryptoservice/service/EncryptionServiceTest.kt
@@ -1,24 +1,50 @@
 package cryptoservice.service
 
 import cryptoservice.model.GCPAccessToken
-import cryptoservice.service.EncryptionService
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class EncryptionServiceTest {
+    @Disabled
     @Test
-    fun `encrypt data`() {
+    fun `when gcp access token is available data is successfully encrypted`() {
+        val gcpAccessToken = GCPAccessToken("din-access-token")
+        val configWithGCPResourceAndAge = "creation_rules:\n" +
+                "    - key_groups:\n" +
+                "        - age:\n" +
+                "            - \"age1g9m644t5s95zk6px9mh2kctajqw3guuq2alntgfqu2au6fdz85lq4uupug\"\n"+
+                "        - gcp_kms:\n" +
+                "            - resource_id: projects/spire-ros-5lmr/locations/europe-west4/keyRings/rosene-team/cryptoKeys/ros-as-code-2"
+
         val encrypted = EncryptionService().encrypt(
             text = "{\"test\":\"test\"}",
-            config = "creation_rules:\n" +
-                    "    - key_groups:\n" +
-                    "        - age:\n" +
-                    "            - \"age1g9m644t5s95zk6px9mh2kctajqw3guuq2alntgfqu2au6fdz85lq4uupug\"\n"+
-                    "        - gcp_kms:\n" +
-                    "            - resource_id: projects/spire-ros-5lmr/locations/europe-west4/keyRings/rosene-team/cryptoKeys/ros-as-code-2",
-            gcpAccessToken = GCPAccessToken("xxx-din-gcp-token"),
+            config = configWithGCPResourceAndAge,
+            gcpAccessToken = gcpAccessToken,
             riScId = "ad-12314"
         )
 
-        println(encrypted)
+        assertThat(encrypted).isNotNull()
+        assertThat(encrypted).contains("sops")
     }
+
+    @Test
+    fun `when `() {
+        val invalidGcpAccessToken = GCPAccessToken("feil")
+        val configWithGCPResourceAndAge = "creation_rules:\n" +
+                "    - key_groups:\n" +
+                "        - age:\n" +
+                "            - \"age1g9m644t5s95zk6px9mh2kctajqw3guuq2alntgfqu2au6fdz85lq4uupug\"\n"+
+                "        - gcp_kms:\n" +
+                "            - resource_id: projects/spire-ros-5lmr/locations/europe-west4/keyRings/rosene-team/cryptoKeys/ros-as-code-2"
+
+        assertThrows<Exception> { EncryptionService().encrypt(
+            text = "{\"test\":\"test\"}",
+            config = configWithGCPResourceAndAge,
+            gcpAccessToken = invalidGcpAccessToken,
+            riScId = "ad-12314"
+        ) }
+    }
+
 }
