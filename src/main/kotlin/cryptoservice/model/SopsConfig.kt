e021ad04307058dde620aa2324dca658f40b2e65
Håkon Frøland <hakon.froland@gmail.com>
2025-02-18 17:12:24 +0100

    variabelnavn like med json objektet for å hindre feil

--- src/main/kotlin/cryptoservice/model/SopsConfig.kt
+++ src/main/kotlin/cryptoservice/model/SopsConfig.kt
@@ -3,29 +3,29 @@ package cryptoservice.model
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class SopsConfig(
-    @JsonProperty("shamir_threshold") val shamirThreshold: Int,
-    @JsonProperty("key_groups") val keyGroups: List<KeyGroup>? = null,
+    @JsonProperty("shamir_threshold") val shamir_threshold: Int,
+    @JsonProperty("key_groups") val key_groups: List<KeyGroup>,
     @JsonProperty("kms") val kms: List<Any>? = null,
     @JsonProperty("gcp_kms") val gcpKms: List<GcpKmsEntry>? = null,
     @JsonProperty("azure_kv") val azureKv: List<Any>? = null,
     @JsonProperty("hc_vault") val hcVault: List<Any>? = null,
     @JsonProperty("age") val age: List<AgeEntry>? = null,
-    @JsonProperty("lastmodified") val lastModified: String? = null,
+    @JsonProperty("lastmodified") val lastmodified: String? = null,
     @JsonProperty("mac") val mac: String? = null,
     @JsonProperty("pgp") val pgp: List<Any>? = null,
-    @JsonProperty("unencrypted_suffix") val unencryptedSuffix: String? = null,
+    @JsonProperty("unencrypted_suffix") val unencrypted_suffix: String? = null,
     @JsonProperty("version") val version: String? = null,
 )
 
 data class KeyGroup(
-    @JsonProperty("gcp_kms") val gcpKms: List<GcpKmsEntry>? = null,
-    @JsonProperty("hc_vault") val hcVault: List<Any>? = null,
+    @JsonProperty("gcp_kms") val gcp_kms: List<GcpKmsEntry>? = null,
+    @JsonProperty("hc_vault") val hc_vault: List<Any>? = null,
     @JsonProperty("age") val age: List<AgeEntry>? = null,
 )
 
 data class GcpKmsEntry(
-    @JsonProperty("resource_id") val resourceId: String,
-    @JsonProperty("created_at") val createdAt: String? = null,
+    @JsonProperty("resource_id") val resource_id: String,
+    @JsonProperty("created_at") val created_at: String? = null,
     @JsonProperty("enc") val enc: String? = null,
 )
 
