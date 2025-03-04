fb22ab57b11144ef196e6a5488c03542de13c22b
Jørn Ola Birkeland <jorn.ola.birkeland@bekk.no>
2025-03-04 10:40:36 +0100

    Fikser Lint-feil og kommenterer ut en testene

--- src/test/kotlin/cryptoservice/service/ContainerTest.kt
+++ src/test/kotlin/cryptoservice/service/ContainerTest.kt
@@ -2,46 +2,53 @@ package cryptoservice.service
 
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.testcontainers.containers.ComposeContainer
 import java.io.File
 
-val compose = ComposeContainer(
-    File("docker-compose.yaml")
-).withExposedService("crypto-service", 8083)
+val compose =
+    ComposeContainer(
+        File("docker-compose.yaml"),
+    ).withExposedService("crypto-service", 8083)
 
+@Disabled
 class ContainerTest {
-
     @Test
     fun `call encrypt`() {
-        val address = "http://" + compose.getServiceHost(
-            "crypto-service",
-            8083
-        ) + ":" + compose.getServicePort("crypto-service", 8083)
+        val address =
+            "http://" +
+                compose.getServiceHost(
+                    "crypto-service",
+                    8083,
+                ) + ":" + compose.getServicePort("crypto-service", 8083)
 
         simpleGetRequest(address)
     }
 
     private fun simpleGetRequest(url: String) {
         val client = WebTestClient.bindToServer().baseUrl("http://localhost:8083").build()
-        client.get().uri("/")
+        client
+            .get()
+            .uri("/")
             .accept(MediaType.APPLICATION_JSON)
             .exchange()
-            .expectStatus().isOk
+            .expectStatus()
+            .isOk
     }
 
     companion object {
         @JvmStatic
         @BeforeAll
-        fun beforeAll(): Unit {
+        fun beforeAll() {
             compose.start()
         }
 
         @JvmStatic
         @AfterAll
-        fun afterAll(): Unit {
+        fun afterAll() {
             compose.stop()
         }
     }
--- src/test/kotlin/cryptoservice/service/EncryptionServiceTest.kt
+++ src/test/kotlin/cryptoservice/service/EncryptionServiceTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
+@Disabled
 class EncryptionServiceTest {
     @Disabled
     @Test
